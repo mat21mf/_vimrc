2b4946160f34b8cb0d2a571cea469b0b2471e1b4
Matias Rebolledo <matias.rebolledo@gmail.com>
2024-07-01 15:40:43 +0000

    Edit bloques setup.

--- config-vim.sh
+++ config-vim.sh
@@ -1,5 +1,5 @@
 
-
+## load directories
 if [[ ! -d ~/.vim ]] ; then mkdir -p ~/.vim ; fi
 
 if [[ ! -f ~/.vim/.vimrc ]] ; then cp -R -p -u .vimrc   ~/.vim/.vimrc ; fi
@@ -9,18 +9,19 @@ if [[ ! -d ~/.vim/spell  ]] ; then cp -R -p -u spell    ~/.vim/spell ; fi
 if [[ ! -d ~/.vim/backup ]] ; then mkdir -p  ~/.vim/backup ; fi
 if [[ ! -d ~/.vim/tmp    ]] ; then mkdir -p  ~/.vim/tmp ; fi
 
-# vundle
+## load vimrc
+if [[ ! -f ~/.vimrc ]] ; then ln -s ~/.vim/.vimrc ~/.vimrc ; fi
 
+## setup vundle
 if [[ ! -d ~/.vim/bundle/Vundle ]] ; then
     cd ~/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim
 fi
 
-# all plugins
+## all plugins
 vim -c 'PluginInstall' -c 'qa!'
 
-# ycm
-
+## ycm
 sudo apt install --no-install-recommends cmake python3-dev build-essential
 if [[ ! -d ~/.vim/bundle/YouCompleteMe ]] ; then
     cd ~/.vim/bundle/
